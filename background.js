0bb8c70136fc011dd5ab15f2a654f1eebc95418f
jiashuang <jias.tw@gmail.com>
2017-03-25 08:19:05 +0800

    refine background.js

--- background.js
+++ background.js
@@ -1,18 +1,30 @@
 var tabId = false;
+var tabURL = null;
 
-function onTabCallback(tab) {
+function onTabCreateCallback(tab) {
     tabId = tab.id;
+}
+
+function onTabGetCallback() {
+    if (chrome.runtime.lastError) {
+        chrome.tabs.create({url: tabURL}, onTabCreateCallback);
+    } else {
+        chrome.tabs.update(tabId, {url: tabURL});
+    }
 }
 
 function onForward () {
     var prefix = localStorage.getItem('prefix');
     if (prefix && prefix.length > 0) {
         chrome.tabs.getSelected(null, function(tab) {
-            var tabURL = prefix + tab.url;
-            if (tabId === false) {
-                chrome.tabs.create({url: tabURL}, onTabCallback);
-            } else {
-                chrome.tabs.update(tabId, {url: tabURL});
+            tabURL = prefix + tab.url;
+            var result = confirm(tabURL);
+            if (result) {
+                if (tabId === false) {
+                    chrome.tabs.create({url: tabURL}, onTabCreateCallback);
+                } else {
+                    chrome.tabs.get(tabId, onTabGetCallback);
+                }
             }
         });
     }
